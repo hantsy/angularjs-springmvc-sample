b1eb7c1041676ab35654265d389c7cbfd23dfc4e
hantsy <hantsy@gmail.com>
2016-01-17 21:16:03 +0800

    upgraded springfox to 2.3.1

--- src/main/java/com/hantsylabs/restexample/springmvc/config/SwaggerConfig.java
+++ src/main/java/com/hantsylabs/restexample/springmvc/config/SwaggerConfig.java
@@ -25,30 +25,30 @@ public class SwaggerConfig {
     @Bean
     public Docket postsApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-            .groupName("public-api")
-            .apiInfo(apiInfo())
-            .select()
-            .paths(postPaths())
-            .build();
+                .groupName("public-api")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(postPaths())
+                .build();
     }
 
     private Predicate<String> postPaths() {
         return or(
-            regex("/api/posts.*")
+                regex("/api/posts.*"),
+                regex("/api/comments.*")
         );
     }
 
-
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-            .title("SpringMVC Example API")
-            .description("SpringMVC Example API reference for developers")
-            .termsOfServiceUrl("http://hantsy.blogspot.com")
-            .contact("Hantsy Bai")
-            .license("Apache License Version 2.0")
-            .licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE")
-            .version("2.0")
-            .build();
+                .title("SpringMVC Example API")
+                .description("SpringMVC Example API reference for developers")
+                .termsOfServiceUrl("http://hantsy.blogspot.com")
+                .contact("Hantsy Bai")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE")
+                .version("2.0")
+                .build();
     }
 
 }
