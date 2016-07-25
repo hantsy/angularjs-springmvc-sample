9b8ee4db7d1986618e665377d13b324a972234bb
hantsy <hantsy@gmail.com>
2016-07-25 14:12:01 +0800

    updated the legacy Contact api in SwaggerConfig

--- src/main/java/com/hantsylabs/restexample/springmvc/config/SwaggerConfig.java
+++ src/main/java/com/hantsylabs/restexample/springmvc/config/SwaggerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import static springfox.documentation.builders.PathSelectors.regex;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -44,7 +45,7 @@ public class SwaggerConfig {
                 .title("SpringMVC Example API")
                 .description("SpringMVC Example API reference for developers")
                 .termsOfServiceUrl("http://hantsy.blogspot.com")
-                .contact("Hantsy Bai")
+                .contact(new Contact("Hantsy Bai", "http://hantsy.blogspot.com", "hantsy@gmail.com"))
                 .license("Apache License Version 2.0")
                 .licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE")
                 .version("2.0")
